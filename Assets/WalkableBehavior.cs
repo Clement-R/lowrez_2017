52717daf3a25c95df9c829b9094d905c03c3aba7
C.Rondeau <rondeau.clement@gmail.com>
2017-08-09 19:23:01 +0200

    Work on customer animation and animator

---
+++ Assets/WalkableBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkableBehavior : MonoBehaviour {
+
+    public Transform targetPosition;
+
+	void Start () {
+        StartCoroutine(WalkTo(targetPosition.position));
+	}
+
+    IEnumerator WalkTo(Vector2 target) {
+        while(transform.position.y != targetPosition.position.y) {
+            transform.Translate(0, 1, 0);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        OnTravelEnd();
+    }
+
+    void OnTravelEnd() {
+
+    }
+}
