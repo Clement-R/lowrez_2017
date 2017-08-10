e2abda4ec312c73ecb9a9b115eaa62c4aebee3c2
C.Rondeau <rondeau.clement@gmail.com>
2017-08-10 11:33:55 +0200

    Add scene selector to switch between different stations

---
+++ Assets/DebugCameraMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCameraMove : MonoBehaviour {
+
+    SceneSelector sceneManager;
+
+	void Start () {
+        sceneManager = Camera.main.GetComponent<SceneSelector>();
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Alpha1)) {
+            sceneManager.GoToShop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            sceneManager.GoToCook();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            sceneManager.GoToFilling();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4)) {
+            sceneManager.GoToTopping();
+        }
+    }
+}
---
+++ Assets/SceneSelector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSelector : MonoBehaviour {
+
+    public Transform shopScene;
+    public Transform cookScene;
+    public Transform fillingScene;
+    public Transform toppingScene;
+
+    void SetPosition(Vector3 newPosition) {
+        transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+    }
+
+    public void GoToShop() {
+        SetPosition(shopScene.position);
+    }
+
+    public void GoToCook() {
+        SetPosition(cookScene.position);
+    }
+
+    public void GoToFilling() {
+        SetPosition(fillingScene.position);
+    }
+
+    public void GoToTopping() {
+        SetPosition(toppingScene.position);
+    }
+}
