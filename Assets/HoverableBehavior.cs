a776d243e757f7a126effccbd441161d9558e6b2
C.Rondeau <rondeau.clement@gmail.com>
2017-08-10 11:18:48 +0200

    Add sprite outline shader and a script to detect when a sprite is hovered and trigger outline

---
+++ Assets/HoverableBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverableBehavior : MonoBehaviour {
+    // Update is called once per frame
+    void OnMouseEnter() {
+        GetComponent<SpriteOutline>().outlineSize = 1;
+    }
+
+    private void OnMouseExit() {
+        GetComponent<SpriteOutline>().outlineSize = 0;
+    }
+}
