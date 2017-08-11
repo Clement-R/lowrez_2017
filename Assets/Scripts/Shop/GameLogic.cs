f9229bd36347f1f6f847aeec30dceefbc56d4e2c
C.Rondeau <rondeau.clement@gmail.com>
2017-08-11 19:33:42 +0200

    Start to work on LaneManager and GameLogic scripts, to read a level file and send customers to the first available positions

---
+++ Assets/Scripts/Shop/GameLogic.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+
+    private Stack<Order> _orders = new Stack<Order>();
+    private Order _nextOrder = null;
+
+	void Start () {
+        // Get day (level) data
+        TextAsset json = Resources.Load("day1") as TextAsset;
+        Day level = JsonUtility.FromJson<Day>(json.text);
+
+        // Stack customers and send them when a position is available
+        foreach (var order in level.orders) {
+            _orders.Push(order);
+        }
+
+        Debug.Log(LaneManager.instance.GetPositionInLane(LaneManager.instance.positions[0]));
+        Debug.Log(LaneManager.instance.GetPositionInLane(LaneManager.instance.positions[3]));
+        Debug.Log(LaneManager.instance.GetPositionInLane(LaneManager.instance.positions[6]));
+        // StartCoroutine("SendCustomers");
+    }
+
+    IEnumerator SendCustomers() {
+        while(true) {
+
+            // If we doesn't have a waiting order we pick the next one
+            if(_nextOrder == null && _orders.Count > 0) {
+               _nextOrder = _orders.Pop();
+            }
+
+            // Send the customer if it's time of arrival came
+            if(Time.time >= _nextOrder.time) {
+
+                // Search a position for the customer, if none is found then we wait
+                Transform position = LaneManager.instance.GetAvailablePosition();
+                while(position != null) {
+                    position = LaneManager.instance.GetAvailablePosition();
+                    yield return null;
+                }
+
+                // Launch it !
+                // set WalkableBehavior : TargetPosition, ExitPosition
+                // set SpriteRenderer : OrderInLayer
+
+                _nextOrder = null;
+            }
+
+            yield return null;
+        }
+    }
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Shop/LaneManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneManager : MonoBehaviour {
+
+    public static LaneManager instance = null;
+    public Transform[] positions = new Transform[9];
+
+    private bool[] _positionsState = new bool[9];
+
+    void Awake() {
+        // Singleton pattern
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start () {
+        for (int i = 0; i < 9; i++) {
+            _positionsState[i] = true;
+        }
+	}
+
+    public Transform GetAvailablePosition() {
+        Transform availablePosition = null;
+        int id = -1;
+
+        // Search for an available position
+        for (int i = 0; i < _positionsState.Length; i++) {
+            if (_positionsState[i] == false) {
+                id = i;
+                break;
+            }
+        }
+
+        // Return position or null
+        if (id != -1) {
+            availablePosition = positions[id];
+            _positionsState[id] = true;
+        }
+        return availablePosition;
+    }
+
+    public int GetPositionInLane(Transform position) {
+
+        for (int i = 0; i < positions.Length; i++) {
+            if(positions[i] == position) {
+                return (i / 3);
+            }
+        }
+
+        return -1;
+    }
+}
